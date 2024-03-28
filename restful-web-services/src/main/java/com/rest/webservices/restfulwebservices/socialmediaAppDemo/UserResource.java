c9b85bfc3e83990533299753ccc6381b56605da8
Ramu Gudimalla <ramugudimalla@gmail.com>
2024-03-28 15:22:25 +0530

    day9

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/socialmediaAppDemo/UserResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/socialmediaAppDemo/UserResource.java
@@ -3,7 +3,9 @@ package com.rest.webservices.restfulwebservices.socialmediaAppDemo;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @AllArgsConstructor
@@ -15,11 +17,17 @@ public class UserResource {
          return userDaoService.findAll();
      }@RequestMapping(path = "/users/{id}",method = RequestMethod.GET)
      public User retrieveSingleUser(@PathVariable Integer id){
-         return userDaoService.findOne(id);
+         User user = userDaoService.findOne(id);
+         if(user == null){
+             throw new UserNotFoundException("id "+id);
+         }
+        return user;
      }
      @PostMapping("/users")
-    public void createUser(@RequestBody User user){
-         userDaoService.save(user);
-         ResponseEntity.created()
+    public ResponseEntity<User> createUser(@RequestBody User user){
+         User savedUser = userDaoService.save(user);
+         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                  .path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+         return ResponseEntity.created(location).build();
      }
 }
