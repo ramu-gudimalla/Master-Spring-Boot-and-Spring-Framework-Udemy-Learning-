8316833df2f6a807208265396040ca2ce6a09eb0
Ramu Gudimalla <ramugudimalla@gmail.com>
2024-04-03 18:06:03 +0530

    day12

--- npm-first-project/frontend/todo-app/src/components/counter/Counter.jsx
+++ npm-first-project/frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,14 +1,46 @@
 import './Counter.css';
+import { useState } from 'react';
+import {PropTypes} from 'prop-types'
+
 export default function Counter(){
+    const [count, setCount] = useState(0);
+    function incrementCounterParentFunction(){
+        setCount(count+1)
+    }
+    return(
+        <>
+            <span className="totalCount">{count}</span>
+            <CounterButton by={1}/>
+            <CounterButton by={2}/>
+            <CounterButton by={5}/>
+        </>
+    )
+}
+
+function CounterButton({by}){
+    
+    const [count, setCount] = useState(0);
+    
     function incrementCounterFunction(){
-        console.log("increment clicked")
+        setCount(count + by)
+    }
+    function decrementCounterFunction(){
+        setCount(count - by)
     }
     return (
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
-                <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
+                <button className="counterButton" onClick={incrementCounterFunction}>+{by}</button>
+                <button className='counterButton' onClick={decrementCounterFunction}>-{by}</button>
             </div>
         </div>
     );
 }
+
+CounterButton.propTypes = {
+    by: PropTypes.number
+  }
+  CounterButton.defaultProps ={
+    by:1
+  }
