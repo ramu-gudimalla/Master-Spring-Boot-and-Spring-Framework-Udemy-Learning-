4ebc65f1996d343f7c3a1668df00b539165ea3f5
Ramu Gudimalla <ramugudimalla@gmail.com>
2024-04-24 12:45:03 +0530

    day24

--- learn-spring-security/src/main/java/com/springSecurity/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/springSecurity/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -1,10 +1,24 @@
 package com.springSecurity.learnspringsecurity.basic;
 
+import com.nimbusds.jose.RSAKey;
+import com.nimbusds.jose.RSAKey.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -13,6 +27,52 @@ public class BasicAuthSecurityConfiguration {
         httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.httpBasic();
+        httpSecurity.headers().frameOptions().sameOrigin();
         return httpSecurity.csrf().disable().build();
+    }
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        var user = User.withUsername("user")
+//                .password("{noop}password")
+                .password("password")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("USER")
+                .build();
+        var admin = User.withUsername("admin")
+//                .password("{noop}password")
+                .password("password")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("ADMIN")
+                .build();
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public KeyPair keyPair(){
+        try {
+            var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        }
+        catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair){
+        return new RSAKey
     }
 }
